4fc201b766f399feacbc3bcd7b87fffee0c64ce6
Pedro Henrique Santos de Jesus <Pedrorik43@gmail.com>
2025-11-11 12:17:34 -0300

    feat: adicionar componentes de UI (Avatar, Badge, Card) e refatorar Header e SearchInput

--- app/_components/search-input.tsx
+++ app/_components/search-input.tsx
@@ -3,18 +3,21 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const SearchInput = () => {
-    return (
-        <div className="flex itens-center gap-2">
-            <Input
-                type="text"
-                placeholder="Pesquise serviços, ou barbearias"
-                className="border-border rounded-full"
-
-            />
-            <Button variant="default" size="icon" className="border-border rounded-full">
-                <SearchIcon />
-            </Button>
-        </div>
-    );
-}
+  return (
+    <div className="itens-center flex gap-2">
+      <Input
+        type="text"
+        placeholder="Pesquise serviços, ou barbearias"
+        className="border-border rounded-full"
+      />
+      <Button
+        variant="default"
+        size="icon"
+        className="border-border rounded-full"
+      >
+        <SearchIcon />
+      </Button>
+    </div>
+  );
+};
 export default SearchInput;
--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,32 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import Image from "next/image";
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 const Home = () => {
-    return (
-        <div>
-            <Header />
-            <div className="space-y-4 px-5">
-                <SearchInput />
-                <Image
-                    src={banner}
-                    alt="Agende agora!"
-                    sizes="100vw"
-                    className="h-auto w-full"
-                />
-            </div>
-        </div>
-    );
+  return (
+    <main>
+      <Header />
+      <div className="space-y-4 px-5">
+        <SearchInput />
+        <Image
+          src={banner}
+          alt="Agende agora!"
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barberShopName="Barbearia do Zé"
+          barberShopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+          date={new Date()}
+        />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
